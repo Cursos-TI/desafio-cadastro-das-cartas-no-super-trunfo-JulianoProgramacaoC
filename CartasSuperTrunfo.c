a329fa6adba6a28a5e2632408ddec384f7f920d8
JulianoProgramacaoC <julianocv9ferreira@gmail.com>
2025-08-29 03:52:46 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,14 +3,14 @@
 int main() {
    //Carta 1
 char estado1;
-char codigo1(10);
+char codigo1(1);
 char nomeCidade[30];
 int populacao1, pontosTuristicos1;
 float area1, pib1;
 
    //Carta 2
 char estado2;
-char codigo2(10);
+char codigo2(2);
 char nomeCidade[30];
 int populacao2, pontosTuristicos2;
 float area2, pib2;
