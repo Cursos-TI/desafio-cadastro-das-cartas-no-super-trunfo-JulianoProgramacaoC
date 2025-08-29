2d10a228bd67a07faa12175703333dc3b3c78425
JulianoProgramacaoC <julianocv9ferreira@gmail.com>
2025-08-29 04:08:05 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,5 +3,15 @@
 int main(){
 printf("Super Trunfo Novato \n");
 printf("Novo Commit \n");
- 
+
+char Estado;
+char Codigo1 [1];
+char NomeCidade [30];
+int Populacao1;
+float Area1;
+float Pib1;
+int PontosTuristicos;
+
+
+
 }
