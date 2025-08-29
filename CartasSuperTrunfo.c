25a6b0ad5c033d3f7789ef2d9ca3ddc2e41c9e9c
JulianoProgramacaoC <julianocv9ferreira@gmail.com>
2025-08-29 03:58:44 +0000

    Novo COMMIT

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
 
-int main() {
-   //Carta 1
-char estado1;
-char codigo1(1);
-char nomeCidade[30];
-int populacao1, pontosTuristicos1;
-float area1, pib1;
-
-   //Carta 2
-char estado2;
-char codigo2(2);
-char nomeCidade[30];
-int populacao2, pontosTuristicos2;
-float area2, pib2;
-
-
-
-
-    return 0;
+int main(){
+printf("Super Trunfo Novato \n");
+printf("Novo Commit \n");
+ 
 }
