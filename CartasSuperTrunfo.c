e6f3f9e91ca418d5a4b5af32a5b4c967d013879a
JulianoProgramacaoC <julianocv9ferreira@gmail.com>
2025-08-31 22:17:20 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -37,13 +37,13 @@ scanf("%[^\n]s", NomeCidade1);
 printf("População: ");
 scanf("%d", &Populacao1);
 
-printf("Área (em KM²): ");
+printf("Area (em KM²): ");
 scanf("%f", &Area1);
 
 printf("PIB (em bilhões): ");
 scanf("%f", &Pib1);
 
-printf("Número de Pontos Turísticos: ");
+printf("Numero de Pontos Turisticos: ");
 scanf("%d", &PontosTuristicos1);
 
 
@@ -53,19 +53,19 @@ printf("\n Cadastro da Carta 2:\n");
 printf("Estado (A-H)");
 scanf("%c", &Estado2);
 
-printf("Código da Carta (B02): ");
+printf("Codigo da Carta (B02): ");
 scanf("%s", &Codigo2);
 
 printf("Nome da Cidade: ");
 scanf(" %[^\n]s", &NomeCidade2);
 
-printf("População: ");
+printf("Populacao: ");
 scanf("%d", &Populacao2);
 
-printf("Área (em km²): ");
+printf("Area (em km²): ");
 scanf("%f", &Area2);
 
-printf("PIB (em bilhões): ");
+printf("PIB (em bilhoes): ");
 scanf("%f", &Pib2);
 
 printf("Número de Pontos Turísticos: ");
@@ -74,22 +74,22 @@ scanf("%d", &PontosTuristicos2);
 // Exibição - Carta 1
 printf("\nCarta 1:\n");
 printf("Estado: A%c\n", Estado1);
-printf("Código: A01%s\n", Codigo1);
+printf("Codigo: A01%s\n", Codigo1);
 printf("Nome da Cidade: São Paulo %s\n", NomeCidade1);
-printf("População: 1232500%d\n", Populacao1);
-printf("Área: 1521.11%.f km²\n", Area1);
+printf("Populacao: 1232500%d\n", Populacao1);
+printf("Area: 1521.11%.f km²\n", Area1);
 printf("PIB: 699.28%.f bilhões de reais\n", Pib1);
 printf("Número de Pontos Turísticos: 5%d\n", PontosTuristicos1);
 
 // Exibição - Carta 2
 printf("\nCarta 2:\n");
 printf("Estado: B%c\n", Estado2);
-printf("Código: B02%s\n", Codigo2);
+printf("Codigo: B02%s\n", Codigo2);
 printf("Nome da Cidade: Rio de Janeiro%s\n", NomeCidade2);
-printf("População: 6748000%d\n", Populacao2);
-printf("Área: 1200.25%.2f km²\n", Area2);
-printf("PIB: 700.20%.f bilhões de reais\n", Pib2);
-printf("Número de Pontos Turísticos: 3%d\n", PontosTuristicos2);
+printf("Populacao: 6748000%d\n", Populacao2);
+printf("Area: 1200.25%.2f km²\n", Area2);
+printf("PIB: 700.20%.f bilhoes de reais\n", Pib2);
+printf("Numero de Pontos Turísticos: 3%d\n", PontosTuristicos2);
 
 
 return 0;
